b7963a3664f2458ed58ebbce2bf14a6c63fad64f
luccie-cmd <hoedje201@gmail.com>
2024-08-06 13:45:36 +0200

    Update main and util

--- assembler/include/util.h
+++ assembler/include/util.h
@@ -1,13 +1,15 @@
 #pragma once
 #include <stddef.h>
 
-#define luccix_list(T) struct { T* elements; size_t count, capacity; }
-#define luccix_list_len(L) L.count
-#define luccix_list_idx(L, I) L.elements[I]
-#define luccix_list_init(L) L.elements = malloc(sizeof(L.elements[0])); L.capacity = 1; L.count = 0
-#define luccix_list_push(L, E) if(luccix_list_len(L) >= L.capacity) \
+#define luccix_assembler_list(T) struct { T* elements; size_t count, capacity; }
+#define luccix_assembler_list_len(L) L.count
+#define luccix_assembler_list_idx(L, I) L.elements[I]
+#define luccix_assembler_list_init(L) L.elements = malloc(sizeof(L.elements[0])); L.capacity = 1; L.count = 0
+#define luccix_assembler_list_push(L, E) if(luccix_assembler_list_len(L) >= L.capacity) \
                                 { \
                                    L.capacity *= 2; \
                                    L.elements = realloc(L.elements, L.capacity);  \
                               } \
                               L.elements[L.count++] = E
+
+char* readFile(const char* file_path);
--- assembler/src/main.c
+++ assembler/src/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <args.h>
+#include <context.h>
 #include <assert.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv){
-    luccix_args *args = parse_args(argc, argv);
+    luccix_assembler_args *args = parse_args(argc, argv);
     assert(args != NULL);
     if(args->verbose){
         print_args(args);
     }
+    luccix_assembler_context* context = context_from_args(args);
+    free(args);
+    free(context);
     return 0;
 }
