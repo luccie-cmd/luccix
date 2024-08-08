33b7eaef5fa81761e7d01e3b81c636b2732028e0
luccie-cmd <hoedje201@gmail.com>
2024-08-08 15:52:07 +0200

    Switch compiler to clang

--- build.py
+++ build.py
@@ -170,9 +170,9 @@ def buildDir(directory_path: str, out_path: str, extra_args: list[str]=[]) -> in
         if getExtension(file) == "asm":
             code = buildAsm("nasm", file, out_path+'/'+file, "elf", extra_args)
         elif getExtension(file) == "c":
-            code = buildC("gcc", file, out_path+'/'+file, extra_args)
+            code = buildC("clang", file, out_path+'/'+file, extra_args)
         elif getExtension(file) == "cc" or getExtension(file) == "cpp":
-            code = buildCXX("g++", file, out_path+'/'+file, extra_args)
+            code = buildCXX("clang++", file, out_path+'/'+file, extra_args)
         else:
             print(f"Invalid or unhandled extension for {file} {getExtension(file)}")
             callCmd(f"rm -f /tmp/{basename}/cache/{file}")
